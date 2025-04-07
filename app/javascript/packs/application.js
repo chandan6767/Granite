5283f9b31362cd95b05b4673744d2236125212f3
Chandan Kumar Mandal <chandan.mandal@pixelcompute.com>
2025-04-07 14:20:42 +0000

    Set up react environment

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,4 +1,10 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails";
+import { initializeLogger } from "common/logger";
 import "../stylesheets/application.scss";
 import "./controllers";
+
+import { setAuthHeaders } from "apis/axios";
+
+initializeLogger();
+setAuthHeaders();
---
+++ app/javascript/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
+
+const App = () => {
+    return (
+        <Router>
+            <Switch>
+                <Route exact path="/" render={() => <div>Home</div>} />
+                <Route exact path="/about" render={() => <div>About</div>} />
+            </Switch>
+        </Router>
+    );
+};
+
+export default App;
