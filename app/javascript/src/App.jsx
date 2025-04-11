c7a12c93c006c80a0cee167867a555a9fe3194af
Chandan Kumar Mandal <chandan.mandal@pixelcompute.com>
2025-04-11 11:11:09 +0000

    Added show page for a task

--- app/javascript/src/App.jsx
+++ app/javascript/src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import Dashboard from "components/Dashboard";
+import { ShowTask } from "components/Tasks";
 import CreateTask from "components/Tasks/Create";
 import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
@@ -9,6 +10,7 @@ const App = () => (
   <Router>
     <ToastContainer />
     <Switch>
+      <Route exact component={ShowTask} path="/tasks/:slug/show" />
       <Route exact component={CreateTask} path="/tasks/create" />
       <Route exact component={Dashboard} path="/dashboard" />
     </Switch>
